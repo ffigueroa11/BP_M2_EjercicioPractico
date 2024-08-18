179ad5feb23987684023879e7a1c286aaa60e3e7
Francisco Figueroa <francisco.figueroa@hotmail.cl>
2024-08-18 00:36:06 -0400

    se agrega validacion campos

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,42 @@ $(document).ready(function() {
 
     function validaDatosEnviados(nameClass) {
 
+        let textoLabel = ''
+        let contError = 0
+        let cantElem = $(nameClass).length
+
         $(nameClass).each(function() {
 
             if ($(this).is("input[type=text]")) {
                 if ($(this).val().length === 0) {
-                    let label = $(this).data('name')
+                    let label = $(this).attr('name')
                     console.log(label)
+                    textoLabel = textoLabel + label + '<br>'
+
                 }
             }
 
         })
+
+        if (contError > 0) {
+            //variables de mensaje
+            alert_encabezado = '<i class="fa fa-exclamation-triangle red"></i>&nbsp;&nbsp;Error';
+            //mostrar mensaje de error 
+            var desc_error = 'Ingrese o seleccione un valor en los siguientes campos:<br><br>' + textoLabel;
+
+            $.alert({
+                title: alert_encabezado,
+                content: desc_error,
+                confirmButton: alert_btnAceptarTx,
+                confirmButtonClass: alert_btnAceptarClass,
+                cancelButtonClass: alert_btnCancelarClass,
+                closeIcon: true,
+                keyboardEnabled: true,
+                confirm: function() {}
+            });
+
+            return false
+        }
+
     }
 })
